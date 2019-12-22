454e7b6b04eb2ae19617bd73738f1e3d88679dd3
Kyros Dev <55705175+KyrosDev@users.noreply.github.com>
2019-12-22 14:26:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import sys
 from github import Github
 
-thisPath = os.getcwd() #Script path
-yourGitToken = "f1f0b36f63a5942e8b1b7174fe1bd7234fc61fa0" #Insert here your github token
+path = os.getcwd() #Script path
+yourGitToken = "" #Insert here your github token
 git = Github(yourGitToken).get_user()
+username = ""
 
 def create():
     name = sys.argv[1] #Take the name of the project
@@ -12,7 +13,7 @@ def create():
 
     #Check if project exist
     if name not in os.listdir(f"{thisPath}/.."):
-        os.chdir(f"{thisPath}/..") #Change directory
+        CHANGEHERE = os.chdir(f"{path}/../") #Change path after '../'
         os.mkdir(name) #Create project folder
         os.chdir(name) #Change direcotry to project folder
         repo = git.create_repo(name) #Create repo
@@ -20,7 +21,7 @@ def create():
         os.system("touch README.md") #Create README.md file
         os.system("git add .") #Add files
         os.system("git commit -m \"Initial Commit\"") #Commit project
-        os.system(f"git remote add origin git@github.com:KyrosDev/{name}.git") #Add files into repo
+        os.system(f"git remote add origin git@github.com:{username}/{name}.git") #Add files into repo
         os.system("git push -u origin master") #Push everything
     
     #If file already exist print
